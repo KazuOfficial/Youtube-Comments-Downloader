d2a02df8471df2a479722c2befa438a64c534f30
KazuGod <kazuzmafii@gmail.com>
2020-03-18 05:29:51 +0100

    Added --limit argument

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Youtube-Comments-Downloader
+# Youtube Comments Downloader
+
+## Dependencies
+
+* Python 2.7+
+* requests
+
+##Installation
+
+```bash
+git clone https://github.com/KazuOfficial/Youtube-Comments-Downloader
+```
+
+## Packages
+
+You can install packages with
+
+    pip install requests
+
+## Usage
--- youtube.py
+++ youtube.py
@@ -1,40 +1,25 @@
-# -*- coding: utf-8 -*-
+import sys
+import requests
+import argparse
 
-# Sample Python code for youtube.channels.list
-# See instructions for running these code samples locally:
-# https://developers.google.com/explorer-help/guides/code_samples#python
+def main(argv):
+    parser = argparse.ArgumentParser(add_help=False, description=('Youtube Comments Downloader made by Kazu'))
+    parser.add_argument('--help', '-h', action='help', help='Show help message')
+    parser.add_argument('--limit', '-l', type=int, default=100, help='Limit number of comments')
+    args = parser.parse_args(argv)
 
-import os
+    limit = args.limit
 
-import google_auth_oauthlib.flow
-import googleapiclient.discovery
-import googleapiclient.errors
-
-scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
-
-def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
-    api_service_name = "youtube"
-    api_version = "v3"
-    client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
-
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_console()
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
-
-    request = youtube.channels().list(
-        part="snippet,contentDetails,statistics",
-        id="UC_x5XG1OV2P6uZZ5FSM9Ttw"
-    )
-    response = request.execute()
-
-    print(response)
+    try:
+        print("Welcome!")
+        youtubelink = input("Insert a youtube link to the video: ")
+        #CTRL-X = exit
+        #iflimitisstring - error message
+        print("Link is valid, processing...")
+        r = requests.get(url = youtubelink) 
+        data = print(r.text)
+    except ValueError:
+        print("Insert a valid youtube link!")
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
